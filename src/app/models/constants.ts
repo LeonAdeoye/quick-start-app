5fd998d8c0ca47d57d714fe4666f1f39a90a98d2
LeonAdeoye <leon.adeoye@gmail.com>
2020-05-01 23:34:28 +0800

    first commit

---
+++ src/app/models/constants.ts
@@ -0,0 +1,10 @@
+export class Constants
+{
+  public static CONFIGURATION_SERVICE_URL_BASE = "http://localhost:20001";
+  public static MAX_LOG_SIZE = 5000;
+  public static DEFAULT_USER_NAME = "Leon Adeoye";
+  // TODO Update app name here
+  public static APP_NAME = "quick-start-app";
+
+  private constructor() {}
+}
